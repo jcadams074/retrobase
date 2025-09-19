ea559a3c46bcd385f967c613c0b47275a24f1528
Jacob Adams <jcadams074@gmail.com>
2025-09-18 22:06:10 -0500

    Added placeholder screens for Dashboard, Consoles, Games, Profile

---
+++ retrobase/src/features/consoles/ConsolesIndexScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function ConsolesIndexScreen(){
+    return(
+        <View>
+            <Text>Consoles Index Screen</Text>
+        </View>
+    );
+}
