ffac5a1f3946d8f12f2adfa63ad2c208757362bd
Jacob Adams <jcadams074@gmail.com>
2025-09-20 01:09:59 -0500

    Configured the safe area view in App() and add flatlist to console index

--- retrobase/src/App.tsx
+++ retrobase/src/App.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <AppNavigator/>
+    <SafeAreaProvider>
+      <SafeAreaView style ={{flex:1}}>
+        <AppNavigator/>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
--- retrobase/src/features/consoles/ConsolesIndexScreen.jsx
+++ retrobase/src/features/consoles/ConsolesIndexScreen.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import ConsoleCard from './components/ConsoleCard';
+import { FlatList } from 'react-native-gesture-handler';
 
+const data = [
+    {id: 'atari_2600'},
+    {id: 'nes'},
+    {id: 'genesis'},
+    {id: 'snes'}, 
+    {id: 'n64'},
+    {id: 'ps1'},
+    {id: 'ps2'},
+    {id: 'gamecube'},
+    {id: 'ps3'},
+]
+   
 export default function ConsolesIndexScreen(){
     return(
         <View style={styles.container}>
-            <ConsoleCard imgSrc={require('./assets/logos/snes_logo.png')} title={'Super Nintendo Entertainment System'}/>
-            <ConsoleCard imgSrc={'hello'} title={'Nintendo Entertainment System'}/>
+            <FlatList
+                data={data} 
+                renderItem={({item}) => <ConsoleCard title={item.title}/>}
+                keyExtractor={item => item.id}           
+                numColumns={2} 
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
-        flexDirection: 'row', 
-        flexWrap: 'wrap'
+       
     }
 });
